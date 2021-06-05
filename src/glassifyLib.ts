9f5929d041cbe180db530309b7675ea1f04ae03e
hoangdv <hoang.dv@outlook.com>
2021-06-05 23:37:35 +0900

    Add glasses for detected faces

--- src/glassifyLib.ts
+++ src/glassifyLib.ts
@@ -2,6 +2,7 @@ import '@tensorflow/tfjs-node';
 import * as canvas from 'canvas';
 import * as faceapi from 'face-api.js';
 import path from 'path';
+import Jimp from 'jimp';
 
 const { Canvas, Image, ImageData } = canvas;
 faceapi.env.monkeyPatch({ Canvas: Canvas as any, Image: Image as any, ImageData });
@@ -17,11 +18,30 @@ class GlassifyLib {
       new faceapi.SsdMobilenetv1Options({ minConfidence: 0.5 }),
     ).withFaceLandmarks();
 
-    const output = faceapi.createCanvasFromMedia(image);
-    faceapi.draw.drawDetections(output, faces.map(res => res.detection));
-    faceapi.draw.drawFaceLandmarks(output, faces.map(res => res.landmarks));
+    const source = await Jimp.read(imageBuffer);
+    const glasses = await Jimp.read(path.join(__dirname, '../', 'public/images/glasses.png'));
 
-    return (output as any).toBuffer('image/jpeg');
+    for (const face of faces) {
+      const { landmarks } = face;
+      const leftEye = landmarks.getLeftEye()[0];
+      const rightEye = landmarks.getRightEye()[4];
+      const nose = landmarks.getNose()[0];
+
+      const newWidth = Math.sqrt(
+        Math.pow(leftEye.x - rightEye.x, 2) +
+        Math.pow(leftEye.y - rightEye.y, 2)
+      );
+
+      const editedGlasses = glasses.clone().resize(newWidth + newWidth / 3, Jimp.AUTO);
+
+      source.composite(
+        editedGlasses,
+        nose.x - editedGlasses.getWidth() / 2,
+        nose.y - editedGlasses.getHeight() / 2,
+      );
+    }
+
+    return source.getBufferAsync('image/jpeg');
   }
 
   private async loadModels() {
